3e1b1b0ad985b1b983ea81edabfa4ec28c142dcf
Matt <marberrym@gmail.com>
2018-09-24 14:30:30 -0400

    Working on the skeleton of the app.  Killing this shit right hurrrrr

---
+++ src/Page Components/HeadLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+let HeadLogo = () =>
+    <div>
+        <img src="./images/DIY.png"/>    
+    </div>
+
+export default HeadLogo;
---
+++ src/Page Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let NavBar = () =>
+    <div className="navBar">
+        <div>About</div>
+        <div>
+            <a>Log In!</a>
+            <a>Sign Up!</a>
+        </div>
+    
+    </div>
+
+export default NavBar;
