6270f8db3882ce5e1b670f399db44be85f8ea4c0
SomeLinuxGuy <github@sect.net>
2018-09-24 16:38:20 -0400

    seed data begun.

---
+++ src/seeddata.sql
@@ -0,0 +1,16 @@
+
+{ "steps": [ 
+    { "order": 1,
+        "text": "Tie a yellow ribbon round the old oak tree.",
+        "image": "http://sect.net/imagedump/vh3o5gsiudg.jpg"
+    },
+    { "order": 2,
+        "text": "Its been 3 long years.",
+        "image": "http://sect.net/imagedump/biu4bs.jpg"
+    },
+    { "order": 3,
+        "text": "Do you still want me?",
+        "image": "http://sect.net/imagedump/gnargnar.jpg"
+    }
+    ]
+}
