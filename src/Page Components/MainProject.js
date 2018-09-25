51d01f82c244c18a16f8c05d051a06fdcbcf9a41
Matt <marberrym@gmail.com>
2018-09-25 09:44:28 -0400

    Fixed product URL with product screen, returns not found if a product is not found

--- src/Page Components/MainProject.js
+++ src/Page Components/MainProject.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import NavBar from './NavBar';
+import NotFound from '../Screens/NotFound';
 
-let MainProject = (props) =>
-    <div className="flexC">
-        <NavBar />
-        <img className="mainImg" src={props.project.img}/>
-        <h1>{props.project.name}</h1>
-        {props.project.instructions.map(tasklist => <div>{tasklist}</div>)}
+let MainProject = (props) => (
+        props.project ?
+            <div className="flexC">
+                <NavBar />
+                <img className="mainImg" src={props.project.img}/>
+                <h1>{props.project.name}</h1>
+                {props.project.instructions.map(tasklist => <div>{tasklist}</div>)}
+            </div>
+        :
+            <NotFound/>
+        
+    )
+        
 
     
-    </div>
+    
 
 export default MainProject;
--- src/Screens/SingleScreen.js
+++ src/Screens/SingleScreen.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import MainProject from '../Page Components/MainProject';
 
 let SingleScreen = (props) => 
+    
     <MainProject project={props.projects.find(project => project.id === props.match.params.projectid)}/>
 
 let SingleScreenSmart = connect(state => 
