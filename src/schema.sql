5bef14367bd9e05f19177ec7aac1a2e4fe92d777
SomeLinuxGuy <github@sect.net>
2018-09-24 22:41:18 -0400

    some database modifications

--- src/schema.sql
+++ src/schema.sql
@@ -12,12 +12,20 @@ CREATE TABLE diy_projects (
     id SERIAL PRIMARY KEY,
     creation_date TIMESTAMP,
     user_id INTEGER REFERENCES diy_users(id),
-    title varchar(200) NOT NULL,
-    description jsonb,
+    project_title varchar(200) NOT NULL,
+    feature_image_url varchar(200) NOT NULL,
+    project_description TEXT NOT NULL
 );
 
 CREATE TABLE diy_materials (
     project_id INTEGER REFERENCES diy_projects(id),
     shopping_list jsonb,
     link_to_cart varchar(200)
+);
+
+CREATE TABLE diy_steps (
+    project_id INTEGER REFERENCES diy_projects(id),
+    step_order INTEGER,
+    step_text TEXT,
+
 );
