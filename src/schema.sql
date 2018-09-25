5e63cb59ce80d24cc46a74d5130144abef5cc26d
SomeLinuxGuy <github@sect.net>
2018-09-25 13:54:39 -0400

    database schema and seed updated

--- src/schema.sql
+++ src/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE diy_users, diy_projects;
+DROP TABLE diy_users, diy_projects, diy_categories, diy_materials, diy_steps, diy_materialbridge, diy_userfaves, diy_badges;
 
 CREATE TABLE diy_users (
     id SERIAL PRIMARY KEY,
@@ -14,18 +14,44 @@ CREATE TABLE diy_projects (
     user_id INTEGER REFERENCES diy_users(id),
     project_title varchar(200) NOT NULL,
     feature_image_url varchar(200) NOT NULL,
+    time_range INTEGER NOT NULL,
+    cost_range INTEGER NOT NULL,
+    category_id INTEGER REFERENCES diy_categories(id),
     project_description TEXT NOT NULL
 );
 
+CREATE TABLE diy_categories (
+    id SERIAL PRIMARY KEY,
+    category_title varchar(200)
+);
+
 CREATE TABLE diy_materials (
-    project_id INTEGER REFERENCES diy_projects(id),
-    shopping_list jsonb,
-    link_to_cart varchar(200)
+    id serial primary key,
+    title varchar(200),
+    amazon_asin varchar(200)
 );
 
 CREATE TABLE diy_steps (
+    id SERIAL PRIMARY KEY,
     project_id INTEGER REFERENCES diy_projects(id),
     step_order INTEGER,
-    step_text TEXT,
+    step_image_URL varchar(200),
+    step_title varchar(200),
+    step_text TEXT
+);
 
+CREATE TABLE diy_materials_bridge (
+    project_id INTEGER REFERENCES diy_projects(id), 
+    material_id INTEGER REFERENCES diy_materials(id),
+    quantity INTEGER
+);
+
+CREATE TABLE diy_userfaves (
+    user_id INTEGER REFERENCES diy_users(id),
+    project_id INTEGER REFERENCES diy_projects(id)
+);
+
+CREATE TABLE diy_badges (
+    user_id INTEGER REFERENCES diy_users(id),
+    badge INTEGER
 );
