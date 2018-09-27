874f7121b3bc6c8eae38a9c3cea6acdc2beac4c4
SomeLinuxGuy <github@sect.net>
2018-09-27 11:50:14 -0400

    wow...

--- src/pagescreens/App.js
+++ src/pagescreens/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../cssstyles/App.css';
-import NavBar from '../components/NavBar';
 import HeadLogo from '../components/HeadLogo';
 import HomeScreen from './HomeScreen';
 import BrowseButton from '../components/BrowseButton';
@@ -9,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="flexC">
-        <NavBar/>
+
         <HomeScreen /> 
         <HeadLogo />
         <BrowseButton /> 
