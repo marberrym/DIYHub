8f488106eda864f09d4d39ff8cce73f8086199b6
Ian Gornall <ian.gornall@gmail.com>
2018-09-27 17:18:36 -0400

    Added amazon add to cart links.

--- src/components/singleproject/Supplies.js
+++ src/components/singleproject/Supplies.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-let Supplies = (props) =>
+let Supplies = (props) => {
+    let queryString = '';
+    for (let i = 0; i < props.supplies.length; i++){
+        let supply = props.supplies[i];
+        queryString += `ASIN.${i+1}=${supply.amazon_asin}&Quantity.${i+1}=${supply.quantity}&`;
+    }
+    queryString = queryString.slice(0, -1);
+    return (
     <div className="marginTop">
         <div className="stepHead">What You Will Need:</div>
+            <div className="shop-link-container"><a className="shop-link" href={`https://www.amazon.com/gp/aws/cart/add.html?${queryString}`}><img className="amazon-logo" src="https://upload.wikimedia.org/wikipedia/commons/7/70/Amazon_logo_plain.svg" /><br />Add to Cart!</a></div>
             {props.supplies.map(supply => 
             <div>
                 <h3>{supply.title}</h3>
@@ -10,5 +18,7 @@ let Supplies = (props) =>
                 <div>Quantity: {supply.quantity}</div>
             </div>)}
     </div>
+    );
+}
 
 export default Supplies
