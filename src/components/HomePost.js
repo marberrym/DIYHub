d031d3991589310388a3faf51dd6c8bf7714ab93
SomeLinuxGuy <github@sect.net>
2018-09-27 15:31:53 -0400

    fix homepage link

--- src/components/HomePost.js
+++ src/components/HomePost.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import {connect} from 'react-redux';
 
 let HomePost = (props) => {
     return props.featured.project ?
-    <Link to={"/project/" + props.featured} style={{ backgroundImage: 'url(' + props.featured.project.feature_image_url + ')'}} className="noDec flexC feature alignC justC">
+    <Link to={"/project/" + props.featured.project.id} style={{ backgroundImage: 'url(' + props.featured.project.feature_image_url + ')'}} className="noDec flexC feature alignC justC">
         <div className="alignC featureText">FEATURED PROJECT</div>
         <div className="flexC">
             <div className="subText projectTitle">{props.featured.project.project_title}</div>
