eafecba4749d1fcb6ac72539abeda1b26c44a980
SomeLinuxGuy <github@sect.net>
2018-09-27 13:34:20 -0400

    the first bit

--- src/components/HomePost.js
+++ src/components/HomePost.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 let HomePost = (props) =>
-    <Link to={"/project/" + props.project.id} className="noDec flexC feature alignC justC">
+    <Link to={"/project/" + props.featured.id} className="noDec flexC feature alignC justC">
         <div className="alignC featureText">FEATURED PROJECT</div>
         <div className="flexC">
-            <div className="subText projectTitle">{props.project.name}</div>
+            <div className="subText projectTitle">{props.featured.name}</div>
         </div>
     </Link>
 
-export default HomePost;
+export default connect (
+        (state) =>
+        ({featured: state.featured}) )(fetchFeatured(HomePost))
