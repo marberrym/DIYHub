9953c1093e1b134956ebacdf373d690a0469a33a
Ian Gornall <ian.gornall@gmail.com>
2018-09-27 14:44:26 -0400

    Display time and cost ranges.

--- src/components/singleproject/Header.js
+++ src/components/singleproject/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import priceRange from '../priceRange';
+import timeRange from '../timeRange';
 
 let Header = (props) =>
     <div>
@@ -6,8 +8,8 @@ let Header = (props) =>
         <div>By {props.first_name} {props.last_name}</div>
         <img className="mainImg" src={props.feature_image_url}/>
         <div>{props.project_description}</div>
-        <div>Time: {props.time_range}</div>
-        <div>Cost: {props.cost_range}</div>
+        <div>Time: {timeRange[props.time_range]}</div>
+        <div>Cost: {priceRange[props.cost_range]}</div>
     </div>
 
 export default Header;
