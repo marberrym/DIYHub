7042cb021eb60c56ecf3611bc292d8eb8404abb7
Matt <marberrym@gmail.com>
2018-09-30 18:50:10 -0400

    Misc styling on logo

--- src/components/HeadLogo.js
+++ src/components/HeadLogo.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 let HeadLogo = () =>
     <div className="headLogoDiv">
-        <img src="./images/Diyhublogo.png" className="navGear logo" alt="DIY Cog"/>
-        <div className="logoFont">DIYHub</div>    
+        <img src="./images/Diyhublogo.png" className="logo" alt="DIY Cog"/>
+        <div className="logoText">
+            <div className="logoFont">DIYHub</div>
+            <div className="subLogo">Creations & Inspirations</div>
+        </div>    
     </div>
 
 export default HeadLogo;
