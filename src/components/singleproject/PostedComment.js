c7d7fa74f82fa524174662a5919c9e7eacd71cdc
Matt <marberrym@gmail.com>
2018-10-01 11:42:21 -0400

    Working on comments

--- src/components/singleproject/PostedComment.js
+++ src/components/singleproject/PostedComment.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 let PostedComment = (props) =>
-    <div>
+    <div className="commentBox">
         <div>{props.comment.name} commented:</div>
         <div>{props.comment.comment}</div>
         <div>{props.comment.creation_date}</div>
