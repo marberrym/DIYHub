425a47f6238106b266c6785034a096f8c6fa4498
Matt <marberrym@gmail.com>
2018-10-02 12:57:46 -0400

    The post is pulling the votes logged in the backend database

--- routes/getProject.js
+++ routes/getProject.js
@@ -4,7 +4,7 @@ let getProject = (req, res) => {
   let projectId = req.params.id;
   let projectData = {};
   let project = db.one(
-    `SELECT diy_projects.id AS id, first_name, last_name, category_title, creation_date, project_title, feature_image_url, time_range, cost_range, project_description FROM diy_projects INNER JOIN diy_users ON diy_projects.user_id = diy_users.id INNER JOIN diy_categories ON diy_projects.category_id = diy_categories.id WHERE diy_projects.id=${projectId}`
+    `SELECT diy_projects.id AS id, first_name, last_name, votes, creation_date, project_title, feature_image_url, time_range, cost_range, project_description FROM diy_projects INNER JOIN diy_users ON diy_projects.user_id = diy_users.id INNER JOIN diy_categories ON diy_projects.category_id = diy_categories.id WHERE diy_projects.id=${projectId}`
   )
   let steps = db.query(
     `SELECT step_order, step_image_url, step_title, step_text FROM diy_steps WHERE project_id=${projectId} ORDER BY step_order`
--- src/components/singleproject/Header.js
+++ src/components/singleproject/Header.js
@@ -4,7 +4,13 @@ import timeRange from '../timeRange';
 
 let Header = (props) =>
     <div className="headerMainPost">
-        <div className="mainHead">{props.project_title}</div>
+        <div className="mainHead">
+            {props.project_title}
+            <div className="upVotes">
+                <i class="fas fa-arrow-alt-circle-up voteArrow"></i>
+                {`${props.votes} upvotes`}
+            </div>
+        </div>
         <div>By {props.first_name} {props.last_name}</div>
         <div>{props.project_description}</div>
         <div>Time: {timeRange[props.time_range]}</div>
