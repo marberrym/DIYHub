99a32ac1a01437720825986db0c3c18db9715873
Matt <marberrym@gmail.com>
2018-10-04 19:19:15 -0400

    Added styled responsive navbar - it needs some links though, thats for sure.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import url from '../globalVars';
 import windowSize from 'react-window-size';
 
-let NavBar = (props) =>
-    <div className="navBar">
-        <div className="flex">
-            <Link to="/"><img src="./images/DIYHubLogo.png" className="navGear navLogoRepo" alt="DIY Cog"/></Link>
-            <Link to="/" className="navLink">Home</Link>
-            <Link to="/about" className="navLink">About</Link>
-        </div>
-        <div className="flex">
-            {props.user.name ?
-                <div className="navRightSide flex">
-                    <div>
-                        <div className="greeting">Hello {props.user.name}</div>
-                        <img className="avatar" src={`${url}/uploads/avatar/${props.user.avatar}`} />
+class NavBar extends Component { 
+    constructor(props) {
+        super(props)
+        this.state = {
+            screen: 'computer'
+        }
+    } 
+    componentDidUpdate(prevProps) {
+        if(prevProps !== this.props) {
+            if(this.props.windowWidth < 500) {
+                this.setState({screen: 'mobile'})
+            } else {
+                this.setState({screen: 'computer'})
+            }
+        }
+    }
+    render(){
+        return <div className="navBar">
+            <div className="flex">
+                <Link to="/"><img src="./images/DIYHubLogo.png" className="navGear navLogoRepo" alt="DIY Cog"/></Link>
+                <Link to="/" className="navLink">Home</Link>
+                <Link to="/about" className="navLink">About</Link>
+            </div>
+            <div className="flex">
+                {this.props.user.name ?
+                    this.state.screen === 'computer' ?
+                        <div className="navRightSide flex">
+                            <div>
+                                <div className="greeting">Hello {this.props.user.name}</div>
+                                <img className="avatar" src={`${url}/uploads/avatar/${this.props.user.avatar}`} />
+                            </div>
+                            <Link to="/my-projects" className="navLink"> My Projects</Link>
+                            <Link to="/post" className="navLink"> Post Project</Link>
+                            <span className="navLink" onClick={event =>{
+                                localStorage.removeItem('token');
+                                this.props.dispatch({type: "LOGOUT",  empty: ""})
+                                this.props.dispatch({
+                                    type: 'SET_TOAST',
+                                    toast: {
+                                        text: 'You logged out.',
+                                        type: 'info'
+                                    }
+                                });
+                            }}> Log Out</span>
+                        </div>
+                    :
+                        <div className="navRightSide flex">
+                            <div>
+                                <div className="greeting">Hello {this.props.user.name}</div>
+                                <img className="avatar" src={`${url}/uploads/avatar/${this.props.user.avatar}`} />
+                            </div>
+                            <i className="fas fa-bars fa-2x barsIcon"/>
+                        </div>
+                : 
+                    <div className="navRightSide">
+                        <Link to="/signup" className="navLink">Sign Up</Link>
+                        <Link to="/login" className="navLink"> Log In</Link>
                     </div>
-                    <Link to="/my-projects" className="navLink"> My Projects</Link>
-                    <Link to="/post" className="navLink"> Post Project</Link>
-                    <span className="navLink" onClick={event =>{
-                        localStorage.removeItem('token');
-                        props.dispatch({type: "LOGOUT",  empty: ""})
-                        props.dispatch({
-                            type: 'SET_TOAST',
-                            toast: {
-                                text: 'You logged out.',
-                                type: 'info'
-                            }
-                        });
-                    }}> Log Out</span>
-                </div>
-            : 
-                <div className="navRightSide">
-                    <Link to="/signup" className="navLink">Sign Up</Link>
-                    <Link to="/login" className="navLink"> Log In</Link>
-                </div>
             }</div>
-    </div>
+        </div>
+    }}
 
 let NavBarSmart = connect(state => ({user: state.user}))(NavBar)
 
