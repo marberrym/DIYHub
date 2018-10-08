5f861870b5908263b34f85896a6556db97a4b89d
Ian Gornall <ian.gornall@gmail.com>
2018-10-07 22:29:24 -0400

    integrated image uploader by force.

--- src/components/postcomponents/FileInput.js
+++ src/components/postcomponents/FileInput.js
@@ -14,6 +14,12 @@ export default class App extends React.Component {
             pictures: this.state.pictures.concat(picture),
         });
     }
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.pictures[0] !== prevState.pictures[0]) {
+            console.log(this.state.pictures[0]);
+            this.props.update('projectimage', this.state.pictures[0]);
+        }
+    }
 
     render() {
         return (
@@ -21,10 +27,8 @@ export default class App extends React.Component {
                 withIcon={true}
                 withPreview={true}
                 singleImage={true}
-                buttonText='Choose images'
-                onChange={event => {
-                this.onDrop()
-                this.props.update('projectimage', this.state.pictures)}}
+                buttonText='Choose image'
+                onChange={this.onDrop}
                 imgExtension={['.jpg', '.gif', '.png', '.gif']}
                 maxFileSize={5242880}
             />
