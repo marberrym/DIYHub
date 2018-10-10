8b635cde7d11f318efc0cdf6e548bd930cfa3227
Matthew <marberrym@gmail.com>
2018-10-10 11:08:16 -0400

    finished social icons

--- src/components/singleproject/Header.js
+++ src/components/singleproject/Header.js
@@ -63,7 +63,7 @@ let Header = (props) =>
                 </div>
             }
         </div>
-        <SocialIcons/>
+        <SocialIcons title={props.project_title}/>
         <div className="postTraits">Submitted By {props.first_name} {props.last_name}</div>
         <div className="postTraits">Required Time: {timeRange[props.time_range]}</div>
         <div className="postTraits">Material Cost: {priceRange[props.cost_range]}</div>
